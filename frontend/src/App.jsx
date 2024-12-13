61d0fe163d37a123fb99f8ed50fd8d0fc81f9965
ARAVIND SK <ask.aravind1015@gmail.com>
2024-12-13 19:16:36 +0530

    Added themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -25,7 +27,7 @@ function App() {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
